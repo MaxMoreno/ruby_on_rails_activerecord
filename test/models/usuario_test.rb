6864c33497d5243964801ed74864378a620ddb50
Ricardo Sampayo <Ricardo9588@gmail.com>
2013-11-14 23:36:59 -0430

    seed y pruebas unitarias de usuario

--- test/models/usuario_test.rb
+++ test/models/usuario_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UsuarioTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "guardar_usuario" do
+  	user1 = Usuario.new({ apellido: 'Sampayo', nombre: 'Ricardo', nacimiento: Time.zone.parse("1988-05-09"), sexo: 'm' })
+    assert user1.save
+  end
+
+  test "validar_nombre" do
+  	user1 = Usuario.new({ apellido: 'Sampayo', nombre: 'Ricardo', nacimiento: Time.zone.parse("1988-05-09"), sexo: 'm' })
+	assert_equal( user1.nombre, 'Ricardo')  
+  end
+
+  test "prueba_fallida" do
+	assert false
+  end
+
 end
